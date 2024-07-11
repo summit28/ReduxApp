a550ac7281e0a83e2744b5e519cf413e03f0ac8b
summit28 <sumitraskar15@gmail.com>
2024-07-11 12:32:31 +0530

    Starting towards Redux

---
+++ src/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+    return (
+        <div>
+            <h2>Buy Calcito Shoes</h2>
+            <button className="btn btn-primary mx-2">-</button>
+            Add to Card
+            <button className="btn btn-primary mx-2">+</button>
+        </div>
+    )
+}
+
+export default Shop
